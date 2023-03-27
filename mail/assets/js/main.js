b154c3f3fb9be833d64d39bc42c085f9887c0074
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-27 13:39:19 +0200

    struttura ok "MO VADO A MANGIARE!"

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -1,12 +1,20 @@
 let listaUtenti = ["leonardo@gmail.com", "salame@gmail.com", "boolean@gmail.com", "banana@gmail.com", "briscola@hotmail.com", "misonostufato@gmail.com"]
 
-console.log (listaUtenti[1])
-
-let nome = "salame@gmail.com"
+console.log (listaUtenti[1]);
+ 
+let controllore = false;
+let nome = "leonardo@gmail.com";
 
 for (let x = 0; x < listaUtenti.length; x++) {
 
     if (listaUtenti[x] === nome){
-        console.log ("ok")
-    }
+        controllore = true
   }
+}
+
+if (controllore) {
+    console.log ("daje")
+} else {
+    console.log ("no")
+}
+  
