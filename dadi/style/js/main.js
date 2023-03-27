2b395501abf75eea99f3ea347214e24f87ec771e
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-27 16:57:42 +0200

    DADI completo il primo che arriva a 10 (solo console)

--- dadi/style/js/main.js
+++ dadi/style/js/main.js
@@ -6,10 +6,12 @@ let punteggioComputer=0 ;
 let punteggioGiocatore=0 ;
 let partitegiocate = 0;
 
-    for (let x = 1; x <= 10; x++)  {
 
+for (let x = 1; x <= 10; x++)  {
+    
     let numeroGiocatore = (Math.floor(Math.random() * 6))+1;
     let numeroComputer = (Math.floor(Math.random() * 6))+1;
+
     console.log(numeroGiocatore,numeroComputer)
     if(numeroComputer < numeroGiocatore) {
         console.log  ("hai vinto!")
@@ -28,5 +30,12 @@ let partitegiocate = 0;
     console.log("partite giocate:" + partitegiocate)
     console.log("tu:" + punteggioGiocatore + " " + "CPU:" + punteggioComputer)
 
+    if (punteggioComputer < 10 && punteggioGiocatore < 10) {
+        x = x-1
+    }  else {
+        x = 10
+    }
+
  }
+
 
